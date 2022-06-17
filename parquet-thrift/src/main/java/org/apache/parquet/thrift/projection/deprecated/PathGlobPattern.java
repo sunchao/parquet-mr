03492950b6723e4847c53d09165754ee1e64854b
Chao Sun <sunchao@apple.com>
2022-06-17 13:59:27 -0700

    Upgrade Hadoop dependency to 3.3.2 (#25)
    Parquet-MR currently depend on Hadoop 2.10.x which lacks several important enhancements for S3, such as [HADOOP-15229](https://issues.apache.org/jira/browse/HADOOP-15229), [HADOOP-18028](https://issues.apache.org/jira/browse/HADOOP-18028), [HADOOP-18103](https://issues.apache.org/jira/browse/HADOOP-18103), as well as [HADOOP-16830](https://issues.apache.org/jira/browse/HADOOP-16830), which requires API changes on the usage side. 
    
    This PR:
    - upgrades the dependency from 2.10.x to 3.3.2 (Apple version)
    - removes several deprecated modules which are supposed to be removed after Parquet 1.12 release.

--- parquet-thrift/src/main/java/org/apache/parquet/thrift/projection/deprecated/PathGlobPattern.java
+++ parquet-thrift/src/main/java/org/apache/parquet/thrift/projection/deprecated/PathGlobPattern.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,8 +17,6 @@
  * under the License.
  */
 package org.apache.parquet.thrift.projection.deprecated;
-
-import org.apache.hadoop.fs.GlobPattern;
 
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -43,16 +41,6 @@ public class PathGlobPattern {
    */
   public PathGlobPattern(String globPattern) {
     set(globPattern);
-  }
-
-  /**
-   * Compile glob pattern string
-   *
-   * @param globPattern the glob pattern
-   * @return the pattern object
-   */
-  public static Pattern compile(String globPattern) {
-    return new GlobPattern(globPattern).compiled();
   }
 
   private static void error(String message, String pattern, int pos) {
